60faea7098ea1f900fc5af5b55a48fc8e3e303fc
👁‍🗨 <1654187-WicopeeDot@users.noreply.gitlab.com>
2021-03-24 13:14:12 +0000

    Add 5 more palettes

---
+++ palettes/keeby.js
@@ -0,0 +1,9 @@
+// https://lospec.com/palette-list/keeby
+module.exports = () => {
+  return [
+    [197, 204, 184],
+    [137, 155, 152],
+    [ 93, 104, 114],
+    [ 51,  51,  67]
+  ];
+};
---
+++ palettes/kirokazegb.js
@@ -0,0 +1,9 @@
+// https://lospec.com/palette-list/kirokaze-gameboy
+module.exports = () => {
+  return [
+    [ 51,  44,  80],
+    [ 70, 135, 143],
+    [148, 227,  68],
+    [226, 243, 228]
+  ];
+};
