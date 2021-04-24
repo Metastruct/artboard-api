ca93fd3c96b4fbc81e157b6ba7afba1d905a1e0d
👁‍🗨 <1654187-WicopeeDot@users.noreply.gitlab.com>
2021-04-24 19:06:56 +0000

    fs.readFileSync returns a Buffer, not a String object

--- palettes/6bitrgb.js
+++ palettes/6bitrgb.js
@@ -6,7 +6,7 @@ module.exports = () => {
   const paletteFile = fs.readFileSync(__dirname + '/6bitrgb.hex');
   let palette = [];
 
-  for (const hex of paletteFile.split('\n')) {
+  for (const hex of paletteFile.toString().split('\n')) {
     const { r, g, b } = colorsys.hex2Rgb(hex);
     palette.push([r, g, b]);
   }
