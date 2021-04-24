f5e6ff73674ed1a7ce3084ca3029e28587189481
👁‍🗨 <1654187-WicopeeDot@users.noreply.gitlab.com>
2021-04-24 19:00:26 +0000

    Add 6 bit RGB palette

---
+++ palettes/6bitrgb.js
@@ -0,0 +1,16 @@
+const colorsys = require('colorsys');
+const fs = require('fs');
+
+// https://lospec.com/palette-list/6-bit-rgb
+module.exports = () => {
+  const paletteFile = fs.readFileSync('./6bitrgb.hex');
+  let palette = [];
+
+  for (const hex of paletteFile.split('\n')) {
+    const { r, g, b } = colorsys.hex2Rgb(hex);
+    palette.push([r, g, b]);
+  }
+
+  return palette;
+};
+
