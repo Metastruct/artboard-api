2babf755eae08887d078d1dc6b63b59505c67f1c
👁‍🗨 <1654187-WicopeeDot@users.noreply.gitlab.com>
2021-04-24 19:04:14 +0000

    Fix

--- palettes/6bitrgb.js
+++ palettes/6bitrgb.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // https://lospec.com/palette-list/6-bit-rgb
 module.exports = () => {
-  const paletteFile = fs.readFileSync('./6bitrgb.hex');
+  const paletteFile = fs.readFileSync(__dirname + '/6bitrgb.hex');
   let palette = [];
 
   for (const hex of paletteFile.split('\n')) {
